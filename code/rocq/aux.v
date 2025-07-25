52191f669884fc2fabb07154a66eaa586d34dade
Davide Fissore <fisso9900@gmail.com>
2025-07-25 09:52:15 +0200

    mv check fixpoints into aux

--- code/rocq/aux.v
+++ code/rocq/aux.v
@@ -126,3 +126,126 @@ Proof.
     + apply IH1 in H as [[]|?] => //=.
     + apply IH1 in H as [[]|?] => //=.
 Qed. *)
+
+(* 
+
+  (* Definition Gamma := V -> S.
+
+  Fixpoint eat r1 r2 :=
+    match r1, r2 with
+    | arr _ _ r1, arr _ _ r2 => eat r1 r2
+    | arr _ _ r1, _ => r1
+    | _, _ => r1
+    end.
+
+  Fixpoint incl d1 d2 :=
+    match d1, d2 with
+    | b Exp, b Exp => true
+    | b (d Func), b (d Func) => true
+    | b (d Func), b (d Pred) => true
+    | arr i l1 r1, arr i l2 r2 => incl l1 l2 && incl r1 r2
+    | arr i l1 _, x => incl l1 x
+    | arr o l1 r1, arr o l2 r2 => incl r1 r2
+    | _, _ => false
+  end.
+
+  Fixpoint min m1 m2 :=
+    match m1, m2 with
+    | b Exp, b Exp => b Exp
+    | b (d Func), _ => m1
+    | b (d Pred), _ => m2
+    | arr i l1 r1, arr i l2 r2 => arr i (max l1 l2) (min r1 r2)
+    | arr o l1 r1, arr o l2 r2 => arr o (min l1 l2) (min r1 r2)
+    | _, _ => m1
+  end
+  with max m1 m2 := match m1, m2 with
+    | b Exp, b Exp => b Exp
+    | b (d Func), _ => m1
+    | b (d Pred), _ => m2
+    | arr i l1 r1, arr i l2 r2 => arr i (min l1 l2) (max r1 r2)
+    | arr o l1 r1, arr o l2 r2 => arr o (max l1 l2) (max r1 r2)
+    | _, _ => m1
+  end.
+
+  Fixpoint infer_input (G: Gamma) tm : S * bool :=
+    match tm with
+    | Code (v V) => (G V, true)
+    | Code (p P) => (G P, true)
+    | Data _ => (b Exp, true)
+    | Comb t1 t2 => 
+      match infer_input G t1 with
+      | (r, false) => (r, false)
+      | (arr o _ x, true) => (x, true)
+      | (arr i l r, true) => 
+        match infer_input G t2 with
+        | (_, false) => (r, false)
+        | (d1, true) => (r, incl d1 l)
+        end
+      | (r, _) => (r, false)
+      end
+    end.
+
+  Fixpoint infer_output (G: Gamma) tm : S * bool :=
+    match tm with
+    | Code (v V) => (G V, true)
+    | Code (p P) => (G P, true)
+    | Data _ => (b Exp, true)
+    | Comb t1 t2 => 
+      match infer_output G t1 with
+      | (r, false) => (r, false)
+      | (arr i _ x, true) => (x, true)
+      | (arr o l r, true) => 
+        match infer_input G t2 with
+        | (_, false) => (r, false)
+        | (d1, true) => (r, incl d1 l)
+        end
+      | (r, _) => (r, false)
+      end
+    end.
+
+  Definition update_gamma (g:Gamma) (v : V) s : Gamma := 
+    fun x => if eqn x v then s else g v.
+
+  Fixpoint assume_input D tm (G : Gamma) : (S * Gamma) :=
+  match tm with
+  | Code (v V) => (D, update_gamma G V (min (G V) D))
+  | Code (p P) => (G P, G)
+  | Data _ => (b Exp, G)
+  | Comb l r => 
+    match assume_input D l G with
+    | (arr i dl dr, G) => 
+      if incl dr D then assume_input dl r G
+      else (D, G)
+    | _ => (D, G)
+    end
+  end.
+
+  Fixpoint assume_output D tm (G : Gamma) : (S * Gamma) :=
+  match tm with
+  | Code (v V) => (D, update_gamma G V (min (G V) D))
+  | Code (p P) => (G P, G)
+  | Data _ => (b Exp, G)
+  | Comb l r => 
+    match assume_output D l G with
+    | (arr o dl dr, G) => 
+      if incl dr D then assume_input dl r G
+      else (D, G)
+    | _ => (D, G)
+    end
+  end.
+
+  Definition check_atom (prog:program) atom '(g, s) :=
+    match atom with
+    | Cut => (g, b (d Func))
+    | Call t => 
+      if infer_input g t is (s',true) then 
+        (snd (assume_output s' t g), max s s') (* not sure about the s' passed to assume_output *)
+      else (g, b (d Pred))
+    end.
+
+  Definition check_entails (prog:program) (G:Gamma) (r:R) : bool :=
+    let premises := r.(premises) in
+    let: (expected_det, G) := assume_input (prog.(sig) r.(head)) r.(head) G in
+    let: (G, body_det) := foldr (check_atom prog) (G,b (d (Func))) premises in
+    if infer_output G r.(head) is (_, true) then incl body_det expected_det else false . *)
+*)
