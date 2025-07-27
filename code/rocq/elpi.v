e82aac2b222dbb18293f28f9cd0c2dd4d5c896ab
Davide Fissore <fisso9900@gmail.com>
2025-07-27 20:00:51 +0200

    wip elpi.v

--- code/rocq/elpi.v
+++ code/rocq/elpi.v
@@ -13,10 +13,6 @@ Module Nur (U : Unif).
     | cut : list (list G) -> G.
   Definition alt := (list G).
 
-  (* Definition next_alt {T:Type} a (k : list G -> list alt -> option  T) :=
-    if a is [:: gl & a] then k gl a
-    else None. *)
-
   Definition save_alt_ca (a : A) alts :=
     match a with
     | Cut => cut alts
@@ -25,7 +21,6 @@ Module Nur (U : Unif).
 
   Definition save_alt a (b: Sigma * R) gs := ([seq save_alt_ca x a | x <- (snd b).(premises)] ++ gs).
   Definition more_alt a bs gs := [seq (save_alt a b1 gs) | b1 <- bs] ++ a.
-
 
   Inductive nur (p: program) : list G ->  list alt -> list alt -> Prop :=
   | StopE a : nur [::] a a
@@ -51,11 +46,12 @@ Module Nur (U : Unif).
   Definition add_cas lA lB : alt :=
     [seq add_ca gl lB | gl <- lA].
 
-  Definition add_alt (lB0 lA lB:list (alt)) : list  alt :=
+  Definition add_alt (lB0 lA lB:list alt) : list  alt :=
     match lA with
     | [::] => [::]
     | x :: xs =>
-      ([seq x ++ y | y <- lB] : list alt) ++ flatten [seq [seq la ++ lb | lb <- lB0] | la <- xs]
+      [seq x ++ y | y <- lB] ++ 
+        flatten [seq [seq la ++ lb | lb <- lB0] | la <- xs]
     end.
 
   Fixpoint state_to_list (A: state) (bt : list alt) : list alt :=
@@ -75,13 +71,6 @@ Module Nur (U : Unif).
       add_alt lB0 lA lB 
     end.
 
-    (* 
-      ((x /\ b) \/ y ) /\a (z \/ w)
-
-      x /\ (z \/ w) \/ (y /\ a) =
-      x /\ z \/ x /\ w \/ y /\ a
-    *)
-
   Goal forall p x y z w s1 s2 a, 
     let f x := (Goal p (Call x)) in
     state_to_list (
@@ -91,8 +80,17 @@ Module Nur (U : Unif).
       [:: [:: call x; call z];
       [:: call x; call w];
       [:: call y; call a]].
-      (* [:: [:: call x]; [:: call z]; [:: call w]; 
-      [:: call y; call a]] *)
+  Proof.
+    move=>/=. by [].
+  Qed.
+
+  Goal forall p z w s1 s2 a, 
+    let f x := (Goal p (Call x)) in
+    state_to_list (
+      And 
+        (Or OK s1 KO) (f a) 
+        (Or (f z) s2 (f w))) [::] = 
+      [:: [:: call z]; [:: call w]].
   Proof.
     move=>/=. by [].
   Qed.
@@ -120,22 +118,25 @@ Module Nur (U : Unif).
     end.
 
   Definition runElpi A :=
-    forall s r b l,
-      runb s A r b -> forall p r1, nur' p (state_to_list A l) r1 -> 
-        same_state_next_alt l s r r1.
+    forall s r b,
+      runb s A r b -> forall p l x xs,
+        state_to_list A l = x :: xs ->
+          exists r1,  nur p x xs r1 /\ same_state_next_alt l s r r1.
   
   Goal @runElpi OK.
   Proof.
     rewrite/runElpi.
-    inversion 1; subst => //=; inversion H1 => //; subst.
-    inversion 1.
-    by case: H7 => _ <-/=.
+    inversion 1; subst => //=; inversion H1 => //p; subst.
+    case: H7 => _ <- l x xs[<-<-].
+    eexists; split.
+      apply: StopE.
+    by move=>/=.
   Qed.
   
   Goal @runElpi KO.
   Proof.
-    rewrite/runElpi => s p r r1 b/=.
-    inversion 1; subst => //.
+    rewrite /runElpi.
+    move=>//=.
   Qed.
   
   Goal @runElpi Top.
@@ -146,13 +147,21 @@ Module Nur (U : Unif).
       case: H2 => ??; subst.
       inversion H3; subst => //.
       case: H8 => ??; subst => /=.
-      by inversion 1.
+      move=> p l x xs [<-<-]; eexists; split.
+        apply: StopE.
+      by [].
     - inversion H1; subst => //.
       case: H3 => ??;subst.
       inversion H4 => //.
     - inversion H1; subst => //.
       case: H4 => ??;subst.
       inversion H5 => //.
+  Qed.
+  
+  Goal @runElpi (And OK KO KO).
+  Proof.
+    rewrite/runElpi.
+    inversion 1; subst => //=.
   Qed.
 
   Lemma state_to_list_dead {A l}: state_to_list (dead A) l = [::].
@@ -220,7 +229,15 @@ Module Nur (U : Unif).
       move=>/=.
       by eexists.
     Qed.
-  
+
+  Lemma success_next_alt_none {s A l}: success A -> next_alt s A = None -> 
+    state_to_list A l = [::].
+  Proof.
+    elim: A s l => //.
+    - move=>/=.
+    
+  Abort.
+
   Lemma next_alt_none_state_to_list {s B}:
       failed B -> next_alt s B = None -> 
         forall l, state_to_list B l = [::].
@@ -251,26 +268,29 @@ Module Nur (U : Unif).
         rewrite fA.
         case X: next_alt => [[s3 C]|].
           case: ifP => // fB0 _.
-          rewrite HB0///add_alt/=.
-          case Y: state_to_list => //[x xs].
-          rewrite flatten_empty cats0.
-
-          rewrite HA//.
+          rewrite /add_alt.
+          case Y: state_to_list => //[y ys].
+          rewrite HB0//=?flatten_empty?cats0.
+            admit.
           admit.
-      rewrite HA//.
-    - move=> /andP[sA fB].  
+        rewrite HA//.
+      move=> /andP[sA fB].
       rewrite success_failed//.
-      case X: next_alt => [[]|]//.
-      case Y: next_alt => [[s2 C]|]//.
+      case X: next_alt => [[s1 C]|]//.
+      rewrite HB//; clear HB.
+      case Y: next_alt => [[s1 C]|]//.
         case: ifP => // fB0.
+        rewrite HB0/add_alt//=.
+          case: state_to_list => //_ ?.
+          rewrite flatten_empty//.
         admit.
-      by rewrite HB///add_alt/=flatten_empty.
-  Qed.
+      move=> _; rewrite /add_alt//=.
+      case: state_to_list => //[_ lA].
+      admit.
+  Admitted.
 
-      
-
-  (* Lemma next_alt_failed_state_to_list {s B s3 D}:
-    failed B ->  next_alt s B = Some (s3, D) -> 
+  Lemma next_alt_failed_state_to_list {s B s3 D}:
+    failed B -> next_alt s B = Some (s3, D) ->
       forall l, state_to_list B l = state_to_list D l.
   Proof.
     elim: B s s3 D => //.
@@ -286,79 +306,119 @@ Module Nur (U : Unif).
         move=> [_ <-]/=.
         by rewrite (HA _ _ _ fA X).
       case: ifP => /eqP dB//.
+      have:= (next_alt_none_state_to_list fA X).
+      move=>->/=.
       case: ifP => fB.
         case Y: next_alt => //[[s3 D]][_ <-]/=.
-        rewrite state_to_list_dead/=(HB _ _ _ fB Y). *)
-
+        rewrite state_to_list_dead/=(HB _ _ _ fB Y)//.
+      move=>[_<-]/=.
+      rewrite state_to_list_dead//.
+    - move=> A HA B0 _ B HB/=s1 s2 C.
+      case: ifP => /eqP//dA.
+      move=>/orP[].
+        move=> fA + l.
+        rewrite fA.
+        case X: next_alt => [[s3 D]|]//.
+        case: ifP => //fB0[_<-]/=.
+        have:= HA _ _ _ fA X => ->.
+        f_equal.
+        admit.
+      move=>/andP[sA fB] + l.
+      rewrite success_failed//.
+      case X: next_alt => [[s3 D]|]//.
+        move=>[_<-]/=.
+        have:= HB _ _ _ fB X => ->//.
+      case Y: next_alt => //[[s3 D]].
+      case: ifP => //fB0[_<-]/=.
+      admit.
+  Admitted.
 
   Goal forall s p, next_alt empty (Or OK s (Goal p Cut)) = Some (s, (Or Dead s (Goal p Cut))).
   Proof. move=>//=. Qed.
 
-  Lemma expand_done:
-    forall p r1 x xs,
-      nur p x xs r1 ->
-    forall s A s1 B l,
+  Lemma expand_done {s A s1 B}:
     expand s A = Solved s1 B ->
+      forall p l x xs,
         state_to_list A l = x :: xs ->
-          same_state_next_alt l s (Done s1 B) r1.
+        exists r1 : seq alt,
+          nur p x xs r1 /\ same_state_next_alt l s (Done s1 B) r1.
   Proof.
-    move=> p r1 x xs H.
-    elim: H; clear => //.
-    + move=> + + A.
-      rewrite /same_state_next_alt/=.
-      {
-        elim: A => //.
-        + by move=> /= ?????[] _ <-[]<-/=.
-        + move=> p []//.
-        + { move=> A HA s B HB a s1 s2 C l.
-          move=> /simpl_expand_or_solved[].
-          - move=> [A' [HA'->]]/=.
-            clear HB.
-            have:= (success_dead1 (proj2 (expand_solved_success HA'))).
-            case: ifP => /eqP// dA' _.
-            remember (state_to_list B l ++ l) as r.
-            have [l']:= expand_solve_state_to_list_cons1 HA' r.
-            case Y: state_to_list => //=[y ys][??]; subst y l'.
-            have:= HA ys _ _ _ r HA' Y.
-            clear HA.
-            case X: next_alt=> //=[[s3 D]|]/=H[].
-              by rewrite -Heqr H.
-            subst ys.
-            move=>/=.
-            case: ifP => /eqP dB.
-              by rewrite dB state_to_list_dead.
-            move=><-.
-            clear a.
-            case: ifP => // fB/=.
-              case Z: next_alt => //[[s3 D]|]/=.
-                rewrite state_to_list_dead//=.
-                admit.
-              admit.
-            rewrite state_to_list_dead//.
-          - admit.
-        }
-        admit.
-      }
-    + all: admit.
+    rewrite /same_state_next_alt.
+    elim: A s s1 B => //.
+    - move=>s s1 B [<-<-]/= p l x xs[<-<-]; clear.
+      exists [::]; repeat constructor.
+    - move=>p[]//.
+    - move=>A HA s B HB s1 s2 C/=.
+      move=> /simpl_expand_or_solved[].
+        clear HB.
+        move=> [A'[HA' ->]]/=p l x xs.
+        have {}HA := HA _ _ _ HA'.
+        have [xs' H] := expand_solve_state_to_list_cons1 HA' (state_to_list B l ++ l).
+        have [r [H1 /=H2]] := HA p _ _ _ H.
+        clear HA.
+        rewrite H/= => -[]? H3;subst.
+        eexists; split.
+          apply: StopE.
+        rewrite /same_state_next_alt/=.
+        have:= success_dead1 (proj2 (expand_solved_success HA')).
+        case: ifP => /eqP// dA' _.
+        inversion H1; subst.
+        move: H2.
+        case X: next_alt => [[s3 D]|]//=>?;subst => //.
+        case: ifP => /eqP.
+          move=>->; rewrite state_to_list_dead => //.
+        move=> dB.
+        case: ifP => fB.
+          case Y: next_alt => [[s3 D]|]//.
+            move=> /=; rewrite state_to_list_dead/=.
+            symmetry.
+            apply: next_alt_failed_state_to_list fB Y _.
+          move=>/=.
+          apply: next_alt_none_state_to_list fB Y _.
+        move=>/=; rewrite state_to_list_dead//.
+      move=>[B' [-> [HB' ->]]]/=.
+      move=> p l x xs.
+      rewrite dead_dead_same eqxx state_to_list_dead/=.
+      move=> H.
+      case X: next_alt => [[s3 D]|]//=.
+        rewrite state_to_list_dead/=.
+        clear HA.
+        have /={}HB := HB _ _ _ HB' p _ _ _ H.
+        rewrite X in HB.
+        apply: HB.
+      exists [::]; split => //.
+      admit.
+    - admit.
   Admitted.
 
-  Lemma runExpandedbDone {s s' A B b l}:
+  Lemma runExpandedbDone {s s' A B b}:
     expandedb s A (Done s' B) b ->
-      forall (p0 : program) (r1 : seq alt),
-        nur' p0 (state_to_list A l) r1 -> same_state_next_alt l s (Done s' B) r1.
+    forall p l x xs,
+    state_to_list A l = x :: xs ->
+    exists r1,
+      nur p x xs r1 /\ same_state_next_alt l s (Done s' B) r1.
   Proof.
     remember (Done _ _) as d eqn:Hd => H.
     elim: H s' B Hd => //; clear.
-    - move=> s s' A A' + s1 B [] ??; subst.
-      move: 
+    - move=> s s' A A' + s1 B [??]; subst.
+      apply: expand_done.
+    - move=> s s' r A B b H1 H2 IH s1 C ?;subst.
+      move=> p l x xs H3.
+      have {}IH:= IH _ _ erefl.
+      admit.
+    - move=> s s' r A B b H1 H2 IH s1 C ?;subst.
+      move=> p l x xs H3.
+      have {}IH:= IH _ _ erefl.
+      admit.
+  Admitted.
 
   Lemma runElpiP: forall A, runElpi A.
   Proof.
     move=> A s r b H.
     elim: H; clear.
-    + move=> s s' A B b H p r1. (* Qui Stop *)
-      admit.
-    + move=> s A B b H H1 p x xs r1. (* Qui Fail*)
+    + move=>  s s' A B b.
+      apply: runExpandedbDone.
+    + move=> s A B b H H1 p x xs r1.
       admit.
     + move=> s s' r A B C b1 b2 b3 HA HB HC IH ?; subst.
       move=> p x xs r1.
