f8874723611d61d284500a954a1456d8a5a5e54e
Davide Fissore <fisso9900@gmail.com>
2026-02-05 19:45:16 +0100

    fix proof

--- theories/paper.v
+++ theories/paper.v
@@ -49,7 +49,7 @@ Lemma tree_to_elpi: forall u p fv A s1 B sF s0,
         nur u p fv x.1 x.2 xs sF (t2l (odflt KO B) s0 [::]).
 (*ENDSNIP: tree_to_elpi *)
 Proof.
-  move=> H1 H2 H3 H4.
+  move=> > H1 H2 H3 H4.
   have [b[fv1 H]] := run_runT H4.
   by apply: elpi_equiv.tree_to_elpi H.
 Qed.
@@ -61,7 +61,7 @@ Lemma elpi_to_tree: forall u p fv s1 s2 a na g,
   exists t1, run u p fv s0 t s2 t1 /\ t2l (odflt KO t1) s0 nilC = na.
 (*ENDSNIP: elpi_to_tree *)
 Proof.
-  move=> H1 H2 H3 H4 H5.
+  move=> > H1 H2 H3 H4 H5.
   have [?[?[?[H6 H7]]]] := elpi_equiv.elpi_to_tree H1 H4 H5; subst.
   repeat eexists.
   by apply: runT_run H6.
