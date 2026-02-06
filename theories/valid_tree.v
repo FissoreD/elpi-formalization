74071f22d5eba4dc8b3ac45c54e796f5d3520e42
Davide Fissore <fisso9900@gmail.com>
2026-02-06 09:58:20 +0100

    minor

--- theories/valid_tree.v
+++ theories/valid_tree.v
@@ -67,7 +67,9 @@ Section valid_tree.
     match s with
     | TA _ | OK | KO => true
     | Or None _ B => valid_tree B
-    | Or (Some A) _ B => valid_tree A && ((B == KO) || B.base_or B)
+    | Or (Some A) _ B => 
+      valid_tree A && 
+          ((B == KO) || B.base_or B)
     | And A B0 B => valid_tree A &&
         if success A then valid_tree B 
         else B == big_and B0
