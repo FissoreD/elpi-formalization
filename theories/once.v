a87f8c50db85092e14ad26d6b760692b6bffa696
Davide Fissore <fisso9900@gmail.com>
2026-02-09 16:49:36 +0100

    minor

--- theories/once.v
+++ theories/once.v
@@ -5,26 +5,6 @@ From det Require Import tree_prop_hard.
 
 From det Require Import check_fo.
 
-Section once.
-  Variable once_sym: P.
-  Definition once := 
-    let X := Tm_V 0 in
-  {| 
-    head := Callable_App (Callable_P once_sym) X;
-    premises := [::call ()]
-    
-  |}
-
-  Definition prog_once p :=
-    p.(rules) = []
-
-
-
-End once.
-
-Definition once := [::cut].
-
-
 Lemma is_det_tail_cut u p s fv l:
   is_det u p s fv (And l [::cut] (And (TA cut) [::] OK)).
 Proof.
@@ -47,3 +27,83 @@ Proof.
     case nl: next_alt => //=[l'][?]; subst. 
     apply: IH erefl.
 Qed.
+
+Section once.
+
+  Variable once_sym: P.
+
+  Definition once_impl := 
+    let X := Tm_V (IV 0) in
+    {| 
+      head := Tm_App (Tm_P once_sym) X;
+      premises := call (X) :: [::cut]
+    |}.
+
+  Definition once_sig := arr i  (b (d Pred)) (b (d Func)).
+
+  Definition once_sigS : sigT :=
+    [fmap].[once_sym <- once_sig].
+
+  Definition no_once (r: seq R) :=
+    forall x, x \in r -> 
+      if callable (head x) is Some hd then hd <> once_sym
+      else true.
+
+  Definition prog_once p :=
+    (p.(sig) = p.(sig) + once_sigS) /\ forall r, 
+      p.(rules) = once_impl :: r /\ no_once r.
+
+  Lemma once_sigP sig:
+    (sig + once_sigS).[? once_sym] = Some once_sig.
+  Proof. by rewrite/once_sigS !FmapE.fmapE eqxx/= fsetU0 in_fset1 eqxx. Qed.
+
+  Lemma bc_once u rs fv s t sig: no_once rs ->
+    exists x y, (bc u {| rules := once_impl :: rs; sig := sig + once_sigS |} fv
+    (Tm_App (Tm_P once_sym) t) s).2 = 
+     ([::(x, call y :: cut :: [::])]).
+  Proof.
+    rewrite/bc [callable _]/=.
+    case X: fresh_rules => [fv' rs'].
+    rewrite /=!FmapE.fmapE/= fsetU0 in_fset1 eqxx.
+    case Y: select => [fv2 rs2]/=.
+    move: X; rewrite/=!push => -[??]; subst.
+    move: Y; rewrite/= eqxx/= in_fnd/=.
+      by rewrite in_fsetU in_fset1 eqxx orbT.
+    move=> HP; rewrite !ffunE/=.
+    case M: matching => /=.
+      rewrite !push/= => -[??]; subst.
+      repeat eexists; f_equal.
+      admit.
+    admit.
+  Admitted.
+
+  Lemma id_det_once u p s fv t:
+    prog_once p ->
+    is_det u p s fv (TA (call (Tm_App (Tm_P once_sym) t))).
+  Proof.
+    case: p => -[|r rs] sig []//= HS; first by move=> /(_ [::]) [].
+    move=> /(_ rs) [[?] H]; subst.
+    rewrite/is_det HS => b s' t' fv' Hx.
+    inversion Hx; clear Hx; subst => //.
+    move: H1; rewrite/= !push/= => -[???]; subst.
+    move: H3.
+    set PR := {|rules := _; sig := _|}.
+    have [x[y Hx]] := bc_once u fv s t sig H.
+    rewrite Hx => Hy; inversion Hy; clear Hy; subst => //.
+    rewrite rew_pa in H1.
+    move: H2 => /= => -[?]; subst.
+    inversion H3; clear H3; subst => //.
+    move: H4; rewrite/=!push/=.
+    set BC := bc _ _ _ _ _.
+    set A:= And _ _ _.
+    move=> [???]; subst.
+    have:= run_same_structure H6.
+    destruct t' => //=; destruct t0 => //= /andP[/eqP?/eqP?]; subst.
+    have ? := run_or0 H6; subst.
+    have/=:= run_ko_left2 u PR BC.1 fv' s0 A (Some t0) s' s.
+    move=> /proj2/(_ H6) [b1 Hr].
+    by have:= is_det_tail_cut Hr.
+  Qed.
+
+
+End once.
--- theories/paper.v
+++ theories/paper.v
@@ -8,16 +8,16 @@ From det Require Import t2l valid_tree elpi elpi_equiv.
 
 (*prooftree: runbp*)
 (*SNIP: run_sig *)
-Inductive run (u:Unif) (p : program): fvS -> Sigma -> tree -> 
+Inductive runT (u:Unif) (p : program): fvS -> Sigma -> tree -> 
                   Sigma -> option tree -> Prop :=
 (*ENDSNIP: run_sig *)
-  | run_done s1 s2 A B fv            : success A -> get_subst s1 A = s2 -> (next_alt true A) = B -> run fv s1 A s2 B
-  | run_step  s1 s2 r A B fv0 fv1 st : path_atom A -> step u p fv0 s1 A = (fv1, st, B) -> run fv1 s1 B s2 r -> run fv0 s1 A s2 r
-  | run_fail s1 s2 A B r fv0         : failed A -> next_alt false A = Some B -> run fv0 s1 B s2 r -> run fv0 s1 A s2 r.
+  | run_done s1 s2 A B fv            : success A -> get_subst s1 A = s2 -> (next_alt true A) = B -> runT fv s1 A s2 B
+  | run_step  s1 s2 r A B fv0 fv1 st : path_atom A -> step u p fv0 s1 A = (fv1, st, B) -> runT fv1 s1 B s2 r -> runT fv0 s1 A s2 r
+  | run_fail s1 s2 A B r fv0         : failed A -> next_alt false A = Some B -> runT fv0 s1 B s2 r -> runT fv0 s1 A s2 r.
 (*endprooftree: runbp*)
 
 Lemma run_runT u p fv s0 t0 s1 t1:
-  run u p fv s0 t0 s1 t1 -> (exists b fv1, tree.run u p fv s0 t0 (Some s1) t1 b fv1).
+  runT u p fv s0 t0 s1 t1 -> (exists b fv1, tree.run u p fv s0 t0 (Some s1) t1 b fv1).
 Proof.
   elim => >.
   - move=> sA <-<-; repeat eexists.
@@ -30,7 +30,7 @@ Qed.
 
 
 Lemma runT_run u p fv s0 t0 sx t1 b fv1:
-  tree.run u p fv s0 t0 (Some sx) t1 b fv1 -> run u p fv s0 t0 sx t1.
+  tree.run u p fv s0 t0 (Some sx) t1 b fv1 -> runT u p fv s0 t0 sx t1.
 Proof.
   remember (Some sx) as ss eqn:Hss.
   move=> H; elim_run H sx Hss.
@@ -52,7 +52,7 @@ Open Scope my_scope.
 Lemma tree_to_elpi: forall u p s0 t s2 t',
   let fv := vars_tree t `|` vars_sigma s0 in
   valid_tree t ->
-    run u p fv s0 t s2 t' -> 
+    runT u p fv s0 t s2 t' -> 
       exists s1 g a,
         let: na := (t2l (odflt KO t') s0 [::]) in
         t2l t s0 [::] = (s1,g) :: a /\
@@ -69,7 +69,7 @@ Qed.
 Lemma elpi_to_tree: forall u p fv s1 g s2 a na,
   nur u p fv s1 g a s2 na -> 
     forall s0 t, valid_tree t -> t2l t s0 [::] = (s1,g) :: a -> 
-      exists t', run u p fv s0 t s2 t' /\ t2l (odflt KO t') s0 [::] = na.
+      exists t', runT u p fv s0 t s2 t' /\ t2l (odflt KO t') s0 [::] = na.
 (*ENDSNIP: elpi_to_tree *)
 Proof.
   move=> > H1 H2 H3 H4 H5.
