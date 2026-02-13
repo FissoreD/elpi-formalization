7895f0c1eb7768f46bf79d562dbf958adcb3aab6
Davide Fissore <fisso9900@gmail.com>
2026-02-13 14:17:57 +0100

    rename

--- theories/check_fo.v
+++ theories/check_fo.v
@@ -264,16 +264,11 @@ Section check.
       has_cut (step u p sv s A).2.
   Proof. move/step_has_cut_help => /(_ p sv s)[]//->//. Qed.
 
-  Lemma succ_failF_no_alt A: success A = false -> failed A = false -> nilA A = false.
-  Proof. by rewrite/nilA => -> /failedF_next_alt ->//. Qed.
-
-
   Goal forall sP s, det_tree sP (Or (Some OK) s OK) == false.
   Proof. move=> ?? //=. Qed.
 
-  Lemma step_next_alt {sP A} : 
-    det_tree sP A -> success A ->
-      next_alt true A = None.
+  Lemma det_check_next_alt_succ {sP A} : 
+    det_tree sP A -> success A -> next_alt true A = None.
   Proof.
     elim: A => //=.
     - move=> A HA s B HB /andP[nA +]sA.
@@ -286,11 +281,6 @@ Section check.
       rewrite -{1}[det_tree sP A]andbT -fun_if => /andP[? _].
       by rewrite HA.
   Qed.
-
-  Lemma build_na_is_dead {sP A}:
-    det_tree sP A -> success A ->
-      next_alt true A = None.
-  Proof. move=> H2 H3; rewrite (step_next_alt H2)//=. Qed.
 
   Lemma has_cut_next_alt {A R b}: 
     has_cut A -> next_alt b A = Some R -> has_cut R.
@@ -321,7 +311,7 @@ Section check.
   Lemma next_alt_no_alt b A A' : next_alt b A  = Some A' -> success A = b -> nilA A = false.
   Proof. by rewrite /nilA=> + -> => ->. Qed.
 
-  Lemma no_free_alt_next_alt {sP A R b}:
+  Lemma det_check_next_alt {sP A R b}:
     det_tree sP A -> next_alt b A = Some R -> det_tree sP R.
   Proof.
     elim_tree A R b => /=.
@@ -359,7 +349,7 @@ Section check.
   Definition check_program pr := mut_excl u pr && check_rules pr.
   (*ENDSNIP: check_program *)
 
-  Lemma det_tree_big_or s r0 rs:
+  Lemma det_check_big_or_help s r0 rs:
     all (fun x => check_atoms s x.2) (r0 :: rs) ->
     all_but_last (fun x  => has_cut_seq x.2) (r0 :: rs) ->
     det_tree s (big_or r0.2 rs).
@@ -371,7 +361,7 @@ Section check.
     by apply: IH.
   Qed.
   
-  Lemma is_det_big_or pr c fv fv' r0 rs s1:
+  Lemma det_check_big_or pr c fv fv' r0 rs s1:
     vars_tm c `<=` fv ->
     vars_sigma s1 `<=` fv ->
     check_program pr -> tm_is_det (sig pr) c -> 
@@ -392,10 +382,10 @@ Section check.
     rewrite !push/= => /= ++[?]; subst.
     rewrite (bool_irrelevance kP qp') => ++ S.
     rewrite S.
-    by apply/det_tree_big_or.
+    by apply/det_check_big_or_help.
   Qed.
 
-  Lemma step_no_free_alt pr sv s1 A r: 
+  Lemma det_check_step pr sv s1 A r: 
     vars_tree A `<=` sv ->
     vars_sigma s1 `<=` sv ->
     check_program pr -> det_tree pr.(sig) A -> 
@@ -406,7 +396,7 @@ Section check.
     elim_tree A s1.
     - case: t => [|c]//=; rewrite !push/=.
       case bc: bc => //=[fv'[|[s0 r0]rs]]//= V1 V2 H1.
-      by apply: is_det_big_or bc.
+      by apply: det_check_big_or bc.
     - rewrite/=2!fsubUset -andbA => /and3P[S1 S2 S3] S4 /andP[fA]; rewrite !push/= HA//=.
       case: ifP => //= cA; last by move=> /eqP->; rewrite !if_same.
       rewrite !fun_if => /[dup] Hx ->; do 2 case: ifP => //=.
@@ -427,8 +417,10 @@ Section check.
       rewrite /= dB /=.
       case fA: (failed A).
         by rewrite /nilA /sA failed_step//= SA.
-      rewrite (succ_failF_no_alt SA fA) => /andP[+ ->]/=.
-      by case/orP=> [/HA->/= | /[dup]/andP[-> ?] ->]; rewrite ?andbT ?orbT ?if_same.
+      case pA: (path_atom A).
+        rewrite/nilA path_atom_next_alt_id//= => /andP[+ ->]/=.
+        by case/orP=> [/HA->/= | /[dup]/andP[-> ?] ->]; rewrite ?andbT ?orbT ?if_same.
+      
   Qed.
 
   (*SNIP: is_det *)
@@ -445,13 +437,13 @@ Section check.
     rewrite/is_det.
     move=> D1 D2 H1 H2 r [b[v' R]].
     elim_run R H1 H2 D1 D2.
-    - apply: build_na_is_dead H2 sA.
+    - apply: det_check_next_alt_succ H2 sA.
     - have [H3 H4] := vars_tree_step_sub_flow D1 D2 eA.
       apply: (IH H1 _ H3 H4).
-      by apply: step_no_free_alt eA.
+      by apply: det_check_step eA.
     - have D3:= vars_tree_next_alt_sub_flow D1 nA.
       apply: IH => //.
-      by apply/no_free_alt_next_alt/nA.
+      by apply/det_check_next_alt/nA.
   Qed.
 
   (*SNIP: det_check_call *)
