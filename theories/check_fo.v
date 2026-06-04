8541f03fd091dea50825b6fb9c833c83bc421a77
Davide Fissore <fisso9900@gmail.com>
2026-06-04 20:17:59 +0200

    fix

--- theories/check_fo.v
+++ theories/check_fo.v
@@ -251,10 +251,6 @@ Section check.
       case: eqP => pA.
         by rewrite pA//.
       by move => /andP[? db]; rewrite HA//.
-      rewrite/nilA sA.
-      case: eqP => pA.
-        by rewrite pA//.
-      by move => /andP[? db]; rewrite HA//.
   Qed.
 
   Lemma has_cut_prune {A R b}: 
@@ -313,9 +309,6 @@ Section check.
           by case n: nilA => //=; rewrite orbF => /andP[-> //].
         case nA: prune => [A'|] //= + [<-{R}/=].
         rewrite  has_cut_seq_has_cut_big_and det_tree_big_and (prune_no_alt nA)//.
-        rewrite/nilA andbb=> /andP[+ ->]/=.
-        case: eqP => pA//=.
-        by case/orP=> [/HA/(_ nA)->//|/andP[? ->]] => //=; rewrite orbT.
         rewrite/nilA andbb=> /andP[+ ->]/=.
         case: eqP => pA//=.
         by case/orP=> [/HA/(_ nA)->//|/andP[? ->]] => //=; rewrite orbT.
