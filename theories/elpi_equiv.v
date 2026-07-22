444abcedf272e4d78cf0a69a20a8d8aeaf50156a
Davide Fissore <fisso9900@gmail.com>
2026-07-22 13:30:00 +0200

    rename runS_to_runTCZero

--- theories/elpi_equiv.v
+++ theories/elpi_equiv.v
@@ -262,7 +262,7 @@ Proof.
   by apply : runT2S R.
 Qed.
 
-Theorem runS_to_runTCNone:
+Theorem runS_to_runTCZero:
   forall p t s, let v := vars_tm t `|` vars_sigma s in
     runS p v ((s, consG (call t, [::]) [::]) :: [::]) None <-> 
     runT' p v s (TA (call t)) Zero.
